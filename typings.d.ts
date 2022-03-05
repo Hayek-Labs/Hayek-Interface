703f8d023b9da1b23927ca8d658c50d18e954925
nramos0 <xoen000@163.com>
2022-03-05 14:30:37 +0800

    add global types

--- typings.d.ts
+++ typings.d.ts
@@ -14,3 +14,11 @@ declare type OnClickFn<T = Element, E = MouseEvent> = (
 ) => void;
 
 declare type SetState<T> = React.Dispatch<React.SetStateAction<T>>;
+
+declare type SVGComponent = React.FC<React.SVGProps<SVGSVGElement>>;
+
+declare type Option<T> = T | undefined;
+
+declare type Ok<T> = T;
+declare type Err<E> = E;
+declare type Result<T, E> = Ok<T> | Err<E>;
