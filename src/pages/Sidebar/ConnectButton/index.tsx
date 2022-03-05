c8b97cc1009413b253a6f4c0e6bdd1f0befef536
nramos0 <xoen000@163.com>
2022-03-05 21:59:30 +0800

    add simple metamask connect/disconnect

--- src/pages/Sidebar/ConnectButton/index.tsx
+++ src/pages/Sidebar/ConnectButton/index.tsx
@@ -1,4 +1,4 @@
-import { metaMask } from '@/connectors/metamask';
+import { metaMask, hooks } from '@/connectors/metamask';
 import { useCallback, useEffect } from 'react';
 
 const ConnectButton: React.FC = () => {
@@ -6,18 +6,34 @@ const ConnectButton: React.FC = () => {
     void metaMask.connectEagerly();
   }, []);
 
-  const onClick = useCallback(() => {
+  const connect = useCallback(() => {
     metaMask.activate();
   }, []);
 
+  const disconnect = useCallback(() => {
+    metaMask.deactivate();
+  }, []);
+
+  const { useAccount } = hooks;
+  const account = useAccount();
+
   return (
     <div className="w-full p-3 h-12 sm:h-8 xl:h-16 hidden flex-shrink-0 sm:flex flex-col items-center justify-center hover:cursor-pointer">
-      <span
-        className="hover-cursor-pointer text-white rounded-xl bg-black px-4 py-2"
-        onClick={onClick}
-      >
-        Connect
-      </span>
+      {account ? (
+        <span
+          className="hover-cursor-pointer text-white rounded-xl bg-black px-4 py-2"
+          onClick={disconnect}
+        >
+          Disconnect
+        </span>
+      ) : (
+        <span
+          className="hover-cursor-pointer text-white rounded-xl bg-black px-4 py-2"
+          onClick={connect}
+        >
+          Connect
+        </span>
+      )}
     </div>
   );
 };
