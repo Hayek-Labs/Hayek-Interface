a346f75f28bdfbc2a0a8121ed78775d901dface5
nramos0 <xoen000@163.com>
2022-03-05 14:39:02 +0800

    change top countries to top coins

--- src/pages/Home/TopCountries/index.tsx
+++ src/pages/Home/TopCountries/index.tsx
@@ -1,38 +1,43 @@
+import { Coin, coinToLogo } from '@/constants';
 import Icon from '../Icon';
-import Image from '../Image';
 
-const Countrydata = [
-  { name: 'USA', rise: true, value: 21942.83, id: 1 },
-  { name: 'Ireland', rise: false, value: 19710.0, id: 2 },
-  { name: 'Ukraine', rise: false, value: 12320.3, id: 3 },
-  { name: 'Sweden', rise: true, value: 9725.0, id: 4 },
+const CoinData: { coin: Coin; rise: boolean; value: number; id: number }[] = [
+  { coin: 'USDT', rise: true, value: 21942.83, id: 1 },
+  { coin: 'DAI', rise: false, value: 19710.0, id: 2 },
+  { coin: 'BUSD', rise: false, value: 12320.3, id: 3 },
+  { coin: 'USDC', rise: true, value: 9725.0, id: 4 },
 ];
 
 const TopCountries = () => {
   return (
     <div className="flex p-4 flex-col h-full">
       <div className="flex justify-between items-center">
-        <div className="text-white font-bold">Top Countries</div>
+        <div className="text-white font-bold">Top Coins</div>
         <Icon path="res-react-dash-plus" className="w-5 h-5" />
       </div>
-      <div className="">favourites</div>
-      {Countrydata.map(({ name, rise, value, id }) => (
-        <div className="flex items-center mt-3" key={id}>
-          <div className="">{id}</div>
+      {CoinData.map(({ coin, rise, value, id }) => {
+        const Logo = coinToLogo[coin];
 
-          <Image path={`res-react-dash-flag-${id}`} className="ml-2 w-6 h-6" />
-          <div className="ml-2">{name}</div>
-          <div className="flex-grow" />
-          <div className="">{`$${value.toLocaleString()}`}</div>
-          <Icon
-            path={
-              rise ? 'res-react-dash-country-up' : 'res-react-dash-country-down'
-            }
-            className="w-4 h-4 mx-3"
-          />
-          <Icon path="res-react-dash-options" className="w-2 h-2" />
-        </div>
-      ))}
+        return (
+          <div className="flex items-center mt-3" key={id}>
+            <div className="">{id}</div>
+            <Logo className="ml-2 w-6 h-6" />
+            <div className="ml-2">{coin}</div>
+            <div className="flex-grow" />
+            <div className="">{`$${value.toLocaleString()}`}</div>
+            <Icon
+              path={
+                rise
+                  ? 'res-react-dash-country-up'
+                  : 'res-react-dash-country-down'
+              }
+              className="w-4 h-4 mx-3"
+            />
+            <Icon path="res-react-dash-options" className="w-2 h-2" />
+          </div>
+        );
+      })}
+
       <div className="flex-grow" />
       <div className="flex justify-center">
         <div className="">Check All</div>
