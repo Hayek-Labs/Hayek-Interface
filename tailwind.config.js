3684807c93a21537f52ee2ae22c05ed48f68793b
nramos0 <xoen000@163.com>
2022-03-07 22:34:56 +0800

    use recent tailwindcss version

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  // mode: 'jit',
+  // jit document: https://tailwindcss.com/docs/just-in-time-mode
+  content: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.ts'],
+  theme: {},
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
