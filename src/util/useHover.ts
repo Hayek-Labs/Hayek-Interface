bcbf15f8a9d0f8fc9bff8164f6fab96eb8905830
nramos0 <xoen000@163.com>
2022-03-17 15:22:07 +0800

    tidy up useHover code

--- src/util/useHover.ts
+++ src/util/useHover.ts
@@ -1,6 +1,5 @@
 import { MutableRefObject, useState, useRef, useEffect } from 'react';
 
-// hook returns tuple(array) with type [any, boolean]
 export const useHover = <T extends HTMLElement>(): [
   MutableRefObject<T | null>,
   boolean,
@@ -10,26 +9,22 @@ export const useHover = <T extends HTMLElement>(): [
   return [ref, value];
 };
 
-// hook returns tuple(array) with type [any, boolean]
 export const useHoverWithRef = <T extends HTMLElement>(
   ref: MutableRefObject<T | null>,
 ): [boolean] => {
   const [value, setValue] = useState<boolean>(false);
-  const handleMouseOver = (): void => setValue(true);
-  const handleMouseOut = (): void => setValue(false);
-  useEffect(
-    () => {
-      const node = ref.current;
-      if (node) {
-        node.addEventListener('mouseover', handleMouseOver);
-        node.addEventListener('mouseout', handleMouseOut);
-        return () => {
-          node.removeEventListener('mouseover', handleMouseOver);
-          node.removeEventListener('mouseout', handleMouseOut);
-        };
-      }
-    },
-    [ref], // Recall only if ref changes
-  );
+  const handleMouseOver = () => setValue(true);
+  const handleMouseOut = () => setValue(false);
+  useEffect(() => {
+    const node = ref.current;
+    if (node) {
+      node.addEventListener('mouseover', handleMouseOver);
+      node.addEventListener('mouseout', handleMouseOut);
+      return () => {
+        node.removeEventListener('mouseover', handleMouseOver);
+        node.removeEventListener('mouseout', handleMouseOut);
+      };
+    }
+  }, [ref]);
   return [value];
 };
