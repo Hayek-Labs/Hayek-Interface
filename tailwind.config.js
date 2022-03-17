30fb09fb49469dbe451ebb363de94bf126d80dbb
nramos0 <xoen000@163.com>
2022-03-17 15:13:53 +0800

    add hayek theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   // mode: 'jit',
   // jit document: https://tailwindcss.com/docs/just-in-time-mode
   content: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.ts'],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        'hblack-1': '#0e0304',
+        'hblack-2': '#1e1e1e',
+        'hblack-3': '#353535',
+        'hblack-4': '#676767',
+        'hyellow-1': '#f7b91c',
+        'hblue-1': '#2f49d1',
+        'hgreen-1': '#10b981',
+        'hred-1': '#ef4444',
+      },
+    },
+  },
   variants: {
     extend: {},
   },
