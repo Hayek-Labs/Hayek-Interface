1dbc433dbdf80e762081236c07364014fbada9ba
nramos0 <xoen000@163.com>
2022-05-12 03:32:55 +0800

    feat(develop): move Lock HAS card to Gauge page

--- src/pages/Gauge/index.tsx
+++ src/pages/Gauge/index.tsx
@@ -1,13 +1,13 @@
-import PieChart from './PieChart';
+import LockHASCard from '../VeHAS/LockHASCard';
 import VoteCard from './VoteCard';
 
 const Gauge = () => {
   return (
     <div className="w-full h-full flex flex-col lg:flex-row items-center justify-center">
-      <div className="w-full flex flex-col sm:flex-row items-center justify-center sm:h-96 p-4 sm:p-2">
+      <div className="w-full flex flex-col sm:flex-row items-center justify-center sm:h-96 p-4 sm:p-2 gap-5">
+        <LockHASCard />
         <VoteCard />
       </div>
-      <PieChart />
     </div>
   );
 };
